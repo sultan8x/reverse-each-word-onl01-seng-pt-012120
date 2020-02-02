a3d34f51a3529f9f9ae2da01190db0f81a0757ee
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-02 01:19:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
+<<<<<<< HEAD
   array = string.split(" ")
     new_array = []
 
@@ -7,4 +8,9 @@ def reverse_each_word(string)
   end
 
   new_array.join(" ")
+=======
+  array = string.split
+  array.first
+  array.first.reverse
+>>>>>>> 38e2ea6c093f4ad23cdc04fe3f1f935e1eb3d551
 end
